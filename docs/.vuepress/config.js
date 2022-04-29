f88953fbd845fca8bb78a697474cb6ecf3c09c29
mujunlin <faustine923@icloud.com>
2022-04-29 18:34:58 +0800

    Modify settings

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,47 @@
+const { defaultTheme } = require('vuepress')
+const { searchPlugin } = require('@vuepress/plugin-search')
+const { prismjsPlugin } = require('@vuepress/plugin-prismjs')
+
 module.exports = {
     lang: 'zh-CN',
-    title: '你好， VuePress ！',
-    description: '这是我的第一个 VuePress 站点',
-    base: '/doc/'
+    title: '知识库',
+    description: '后端开发知识库',
+    base: '/doc/',
+
+    // 默认主题配置
+    theme: defaultTheme({
+        // Logo
+        logo: 'https://cdn-statics.zmn.cn/_nuxt/img/logo_web.b793f2a.png',
+        // 仓库地址
+        repo: 'faustine8/doc',
+        repoLabel: '报告问题',
+        // 文档路径
+        docsDir: 'docs',
+        editLinkText: '修改此页',
+        lastUpdatedText: '更新时间',
+        contributorsText: '作者',
+        // 导航
+        navbar: [
+            {
+                text: '首页',
+                link: '/',
+            },
+        ],
+    }),
+
+    // 插件
+    plugins: [
+        searchPlugin({
+            locales: {
+                '/': {
+                    placeholder: '搜索',
+                }
+            }
+        }),
+        prismjsPlugin({
+            // 配置项
+            preloadLanguages: ['java', 'javadoc', 'c', 'scala', 'go', 'sql', 'shell-session']
+        }),
+    ],
+
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,3 @@
-# Hello VuePress
+# 后端知识库
+
+
